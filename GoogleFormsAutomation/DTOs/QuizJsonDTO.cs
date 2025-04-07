a8a532072cbaba8574913d118c628ce2c86d87df
Antonio Mauricio <antonioimportant@gmail.com>
2025-04-07 11:09:59 -0300

    feat: adds QuizJsonDTO

---
+++ GoogleFormsAutomation/DTOs/QuizJsonDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GoogleFormsAutomation.App.DTOs
+{
+    public class QuizJsonDTO
+    {
+        [JsonPropertyName("quizTitle")]
+        public string Title { get; set; }
+        [JsonPropertyName("quizDescription")]
+        public string? Description { get; set; }
+    }
+}
