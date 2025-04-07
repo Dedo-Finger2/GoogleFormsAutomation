6bd124a150de330b32b36aacaca464755dadb6bd
Antonio Mauricio <antonioimportant@gmail.com>
2025-04-07 11:35:43 -0300

    feat: adds parsing json

--- GoogleFormsAutomation/Program.cs
+++ GoogleFormsAutomation/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace GoogleFormsAutomation.App
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using GoogleFormsAutomation.App.DTOs;
+
+namespace GoogleFormsAutomation.App
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            string? jsonFilePath;
+
+            do
+            {
+                Console.Write("Path: ");
+                jsonFilePath = Console.ReadLine();
+            } while (jsonFilePath == null || jsonFilePath == string.Empty);
+
+            QuizJsonDTO? dto = new();
+
+            using (StreamReader r = new(jsonFilePath))
+            {
+                string jsonData = r.ReadToEnd();
+
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    Converters =
+                    {
+                        new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
+                    }
+                };
+
+                dto = JsonSerializer.Deserialize<QuizJsonDTO>(jsonData, options);
+            }
+
+            if (dto == null) return;
+
+            Console.WriteLine(dto.Questions[0].Title);
         }
     }
 }
