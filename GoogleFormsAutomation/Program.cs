29c6d402071be2f967d60cfb84eca9ffa1a4ddc6
Antonio Mauricio <antonioimportant@gmail.com>
2025-04-07 10:55:00 -0300

    Add project files.

---
+++ GoogleFormsAutomation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GoogleFormsAutomation.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
